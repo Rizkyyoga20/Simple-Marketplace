be23d39bc23f97b57f85154440a325a5385fd61b
Rizkyyoga20 <rizkyyoga281@gmail.com>
2022-01-09 06:27:07 +0700

    view gambarlagi

--- resources/views/pages.blade.php
+++ resources/views/pages.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="{{ asset('storage/gambar/epasien-logo.jpg')}}">
+    <link rel="icon" href="{{ url('storage/gambar/epasien-logo.jpg')}}">
     <script type="text/javascript" src="{{ asset('storage/js/Chart.js')}}"></script>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
