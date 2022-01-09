b9ae6874ab26f246458eab282923aaaad4c6996c
Rizkyyoga20 <rizkyyoga281@gmail.com>
2022-01-09 07:04:17 +0700

    view gambarlagi

--- resources/views/pages.blade.php
+++ resources/views/pages.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="{{ url('https://github.com/Rizkyyoga20/Simple-Marketplace/blob/master/storage/app/public/web/man.png')}}">
+    <link rel="icon" href="{{ asset('storage/app/public/web/man.png')}}">
     <script type="text/javascript" src="{{ asset('storage/js/Chart.js')}}"></script>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
